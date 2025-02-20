fe48587c12dda5983c31c79f17a5f4d4af7555e2
tonmoy00saha <tonmoy.official.20@gmail.com>
2025-02-20 13:46:38 +0600

    Home page Updated

--- src/Component/Header/Login/Login.jsx
+++ src/Component/Header/Login/Login.jsx
@@ -7,23 +7,36 @@ const Login = () => {
             <Helmet>
                 <title>Login</title>
             </Helmet>
-            <div className='flex pt-10 gap-10 justify-center'>
-                <div className="card bg-slate-400 w-96 shadow-xl">
-                    <div className="card-body">
-                        <h2 className="card-title">Card title!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
                     </div>
-                </div>
-                <div className="card bg-slate-400 w-96 shadow-xl">
-                    <div className="card-body">
-                        <h2 className="card-title">Card title!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from "react-helmet";
-
+import banner1 from '../../src/assets/hire1.jpg'
+import banner2 from '../../src/assets/hire2.jpg'
+import banner3 from '../../src/assets/hire3.png'
 
 const Home = () => {
     return (
@@ -7,7 +9,41 @@ const Home = () => {
             <Helmet>
                 <title>Home</title>
             </Helmet>
-            <h2>Ami home</h2>
+            <div>
+                <div>
+                    <h3 className="text-3xl font-bold text-center py-7">Seamless Online Interviews for Efficient Hiring!</h3>
+                </div>
+                <div className="carousel w-full mt-6">
+                    <div id="slide1" className="carousel-item relative w-full max-h-96">
+                        <img
+                            src={banner1}
+                            className="w-full" />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide4" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide2" className="carousel-item relative w-full max-h-96">
+                        <img
+                            src={banner2}
+                            className="w-full" />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide1" className="btn btn-circle">❮</a>
+                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide3" className="carousel-item relative w-full max-h-96">
+                        <img
+                            src={banner3}
+                            className="w-full" />
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href="#slide2" className="btn btn-circle">❮</a>
+                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
         </div>
     );
 };
